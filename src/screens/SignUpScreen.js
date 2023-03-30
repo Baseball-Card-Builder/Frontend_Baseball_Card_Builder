eacf1b3e17bc7c117a139de5055668460d9b08ba
Eric <eytam619@gmail.com>
2023-03-29 22:17:25 -0400

    feat: implement post for signup

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,44 +1,55 @@
-import { View, Text, StyleSheet } from "react-native";
+import { Alert, View, Text, StyleSheet } from "react-native";
 import { useState } from "react";
 import CustomInput from "../components/CustomInput";
 import CustomButton from "../components/CustomButton";
+import axios from "axios";
 
 const SignUpScreen = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
+  const postSignUp = () => {
+    try {
+      if (!setPassword === setConfirmPassword) {
+        return Alert.alert("Passwords do not match")
+      }
+      axios.post("http://localhost:8080/user/signup", { email, password });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const onRegisterPressed = () => {
-    console.log("Register Pressed!");
+    postSignUp();
+    if (postSignUp) {
+      navigation.navigate("signIn");
+    }
   };
   const onSignInPressed = () => {
-    navigation.navigate("signIn")
+    navigation.navigate("signIn");
   };
 
   return (
-      <View style={styles.root}>
-        <Text>Create Account</Text>
+    <View style={styles.root}>
+      <Text>Create Account</Text>
 
-        <CustomInput 
-          placeholder="Email" 
-          value={email} 
-          setValue={setEmail} 
-        />
-        <CustomInput
-          placeholder="Password"
-          value={password}
-          setValue={setPassword}
-          secureTextEntry
-        />
-         <CustomInput
-          placeholder="Confirm password"
-          value={confirmPassword}
-          setValue={setConfirmPassword}
-          secureTextEntry
-        />
-        <CustomButton text="Register" onPress={onRegisterPressed} />
-        <CustomButton text="Have an account? Sign in" onPress={onSignInPressed} />
-      </View>
+      <CustomInput placeholder="Email" value={email} setValue={setEmail} />
+      <CustomInput
+        placeholder="Password"
+        value={password}
+        setValue={setPassword}
+        secureTextEntry
+      />
+      <CustomInput
+        placeholder="Confirm password"
+        value={confirmPassword}
+        setValue={setConfirmPassword}
+        secureTextEntry
+      />
+      <CustomButton text="Register" onPress={onRegisterPressed} />
+      <CustomButton text="Have an account? Sign in" onPress={onSignInPressed} />
+    </View>
   );
 };
 
@@ -50,5 +61,4 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginTop: "20%",
   },
-
 });
