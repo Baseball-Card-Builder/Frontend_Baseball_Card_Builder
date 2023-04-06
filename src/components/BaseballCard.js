18537bd86080d3c2c9e778b59e6021b2aea63aac
Eric <eytam619@gmail.com>
2023-04-05 21:28:13 -0400

    feat: implement image styles

--- src/components/BaseballCard.js
+++ src/components/BaseballCard.js
@@ -1,22 +1,24 @@
-import { StyleSheet, View, ScrollView, Image } from 'react-native';
-import { Card, Text, Avatar } from 'react-native-elements';
+import { StyleSheet, View, ScrollView, Image, Text } from "react-native";
+import { Card } from "react-native-elements";
 
 const BaseballCard = () => {
   return (
     <ScrollView>
-        <View>
-            <Card>
-                <Avatar source={{ uri: "https://firebasestorage.googleapis.com/v0/b/mlb-card-collection.appspot.com/o/AJudge.jpeg?alt=media&token=150a10b7-66e4-40d8-a9d4-107c0c1f2190"}} />
-                <Card.Divider />
-                <Text>Aaron Judge</Text>
-                <Text>New York Yankees</Text>
-                <Text>HR: 62</Text>
-                <Text>RBI: 133</Text>
-            </Card>
-        </View>
+        <Card style={styles.card}>
+            <Image
+            style={styles.image}
+              source={{
+                uri: "https://firebasestorage.googleapis.com/v0/b/mlb-card-collection.appspot.com/o/AJudge.jpeg?alt=media&token=150a10b7-66e4-40d8-a9d4-107c0c1f2190",
+              }}
+            />
+            <Card.Divider />
+            <Text>Aaron Judge</Text>
+            <Text>New York Yankees</Text>
+            <Text>HR: 62</Text>
+            <Text>RBI: 133</Text>
+        </Card>
     </ScrollView>
-  )
-   
+  );
 };
 
 const styles = StyleSheet.create({
@@ -24,16 +26,22 @@ const styles = StyleSheet.create({
     borderRadius: 20,
   },
   row: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   col: {
     flex: 1,
     marginLeft: 20,
   },
   label: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginTop: 10,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    alignSelf: 'center',
+    marginBottom: 10,
   },
 });
 
